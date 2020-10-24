c7fddb34cf7c7d76f5feb93e5d8fb28ec6896394
Tushar Singla <singlatushar07@gmail.com>
2020-10-24 17:51:51 +0530

    Added a new layer for improved performance

--- classification .py
+++ classification .py
@@ -47,6 +47,11 @@ model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
